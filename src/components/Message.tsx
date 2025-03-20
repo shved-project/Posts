e1b5355a9732031c320e4d63374e2dee2d83f0ae
Швецов Александр <shvitc@gmail.com>
2025-03-20 19:10:40 +0300

    Сделали адаптив компоненту Message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -19,15 +19,17 @@ const Message = () => {
 	return (
 		<AnimatePresence>
 			{message && (
-				<motion.div
-					className={`fixed bottom-8 left-[50%] translate-[-50%] py-3 px-10 rounded-lg shadow-lg text-white max-w-100 text-center ${bgMessage()}`}
-					animate={{y: 0}}
-					initial={{y: 500}}
-					transition={{duration: duration, type: "spring", stiffness: 260, damping: 20}}
-					exit={{y: 500}}
-				>
-					{message.messageText}
-				</motion.div>
+				<div className='fixed bottom-8 left-[50%] translate-[-50%] px-2 w-1/1'>
+					<motion.div
+						className={`py-3 px-10 rounded-lg shadow-lg text-white max-w-120 mx-auto text-center max-sm:px-5 ${bgMessage()}`}
+						animate={{y: 0}}
+						initial={{y: 500}}
+						transition={{duration: duration, type: "spring", stiffness: 260, damping: 20}}
+						exit={{y: 500}}
+					>
+						{message.messageText}
+					</motion.div>
+				</div>
 			)}
 		</AnimatePresence>
 	);
